ad363c25c231be8c330cf8993e63414bf477554e
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-07 13:16:54 +0300

    init

---
+++ src/main/java/org/example/university_phd/UniversityPhdApplication.java
@@ -0,0 +1,13 @@
+package org.example.university_phd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityPhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityPhdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/university_phd/UniversityPhdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.university_phd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityPhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
