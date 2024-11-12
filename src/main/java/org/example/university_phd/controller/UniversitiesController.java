f22ae33ffa34f707166e49da0023b93d63c58513
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-13 00:00:46 +0300

    start UniversitiesController class

--- src/main/java/org/example/university_phd/controller/UniversitiesController.java
+++ src/main/java/org/example/university_phd/controller/UniversitiesController.java
@@ -1,2 +1,28 @@
-package org.example.university_phd.controller;public class UniversitiesController {
+package org.example.university_phd.controller;
+
+import org.example.university_phd.model.University;
+import org.example.university_phd.service.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class UniversitiesController {
+
+    @Autowired
+    private UniversityService universityService;
+
+    @GetMapping("/universities")
+    public Page<University> getUniversitiesPage(
+            @RequestParam(defaultValue = "0") Integer offset,
+            @RequestParam(defaultValue = "10") Integer limit
+    ) {
+        return universityService.getUniversities(PageRequest.of(offset, limit));
+    }
+
 }
