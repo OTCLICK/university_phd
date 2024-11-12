c21ab48821d75fc5ca0015accea9f7d3d83cbe55
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-12 22:12:43 +0300

    write a University class

--- src/main/java/org/example/university_phd/model/University.java
+++ src/main/java/org/example/university_phd/model/University.java
@@ -1,4 +1,91 @@
 package org.example.university_phd.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Arrays;
+
+@Document(collection = "universities")
 public class University {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String type;
+    private String[] departments;
+    private int buildingNumber;
+    private int numberOfStudents;
+    private String phoneNumber;
+
+    public University() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(String[] departments) {
+        this.departments = departments;
+    }
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(int buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", departments=" + Arrays.toString(departments) +
+                ", buildingNumber=" + buildingNumber +
+                ", numberOfStudents=" + numberOfStudents +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
