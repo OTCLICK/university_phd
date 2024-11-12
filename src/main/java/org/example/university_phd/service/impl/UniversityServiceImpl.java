ee4c3dd1dbecfddf0c5a4350f316e32669fa48c4
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-12 23:50:05 +0300

    write a UniversityServiceImpl interface

--- src/main/java/org/example/university_phd/service/impl/UniversityServiceImpl.java
+++ src/main/java/org/example/university_phd/service/impl/UniversityServiceImpl.java
@@ -1,4 +1,58 @@
 package org.example.university_phd.service.impl;
 
-public class UniversityServiceImpl {
+import org.example.university_phd.model.University;
+import org.example.university_phd.repo.UniversityRepository;
+import org.example.university_phd.service.UniversityService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class UniversityServiceImpl implements UniversityService {
+
+    private final UniversityRepository universityRepo;
+
+    public UniversityServiceImpl(UniversityRepository universityRepo) {
+        this.universityRepo = universityRepo;
+    }
+
+    @Override
+    public long countUniversities() {
+        return universityRepo.count();
+    }
+
+    @Override
+    public Page<University> pageUniversitiesMatchingAll(University university, Pageable pageable) {
+        ExampleMatcher matcher = ExampleMatcher.matchingAll();
+        Page<University> universities = universityRepo.findAll(Example.of(university, matcher), pageable);
+        return universities;
+    }
+
+    @Override
+    public University createUniversity(University university) {
+        universityRepo.save(university);
+        return university;
+    }
+
+    @Override
+    public University getUniversity(String id) {
+        return universityRepo.findById(id).orElseThrow(()->new RuntimeException("university not found"));
+    }
+
+    @Override
+    public void deleteUniversity(String id) {
+        universityRepo.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllUniversities() {
+        universityRepo.deleteAll();
+    }
+
+    @Override
+    public void saveAllUniversities(List<University> universities) {
+        universityRepo.saveAll(universities);
+    }
 }
