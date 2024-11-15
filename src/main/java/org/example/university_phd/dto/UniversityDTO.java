5de84119c7aadfa7df7fa89f1d0d88aeedaa9542
OTCLICK <ivan.budko.2004@mail.ru>
2024-11-15 10:49:00 +0300

    add dto

---
+++ src/main/java/org/example/university_phd/dto/UniversityDTO.java
@@ -0,0 +1,13 @@
+package org.example.university_phd.dto;
+
+public class UniversityDTO {
+
+    String id;
+    String name;
+    String type;
+    String[] departments;
+    int buildingNumber;
+    int numberOfStudents;
+    String phoneNumber;
+
+}
